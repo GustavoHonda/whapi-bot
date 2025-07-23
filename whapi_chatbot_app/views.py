a4c5144da9881c4d534c647b14ebf517ca7be2fb
gustavo_honda <gustavo_honda@usp.br>
2025-07-23 13:19:47 -0300

    before first deploy

--- whapi_chatbot_app/views.py
+++ whapi_chatbot_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.urls import path
+from .views import health_check
 
-# Create your views here.
+urlpatterns = [
+    path("healthz", health_check),
+]
